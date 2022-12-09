ff859fc0d2742c7ff106ba8827e974c65a9ea5a7
Naveen Kumar <59390882+Naveenkumar-cmd@users.noreply.github.com>
2022-12-10 00:25:23 +0530

    Create reverse_number.c

---
+++ reverse_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n,sum=0,rem;
+    
+    printf("enter the no. to be reversed:\n");
+    scanf("%d", &n);
+    
+    while(n!=0){
+        rem = n%10;
+        sum = sum*10+rem;
+        n = n/10;
+        
+     }
+     printf("reverse of the num=%d",sum);
+     
+     return 0;
+        
+}
